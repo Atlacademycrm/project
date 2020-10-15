cdb1b8dfbf64db98004d6f6b248d91da10109c33
Seymur Mammadrza <mseymur@gmail.com>
2020-10-15 23:59:40 +0400

    3rd commit

--- src/main/java/com/atlacademy/crm/controller/UserController.java
+++ src/main/java/com/atlacademy/crm/controller/UserController.java
@@ -1,7 +1,7 @@
 package com.atlacademy.crm.controller;
 
 import com.atlacademy.crm.entity.User;
-import com.atlacademy.crm.interfaces.RepositoryConnector;
+import com.atlacademy.crm.service.UserService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
@@ -10,34 +10,34 @@ import java.util.List;
 @Slf4j
 @RestController
 public class UserController {
-    private RepositoryConnector repositoryConnector;
+private UserService userService;
 
-    public UserController(RepositoryConnector repositoryConnector) {
-        this.repositoryConnector = repositoryConnector;
+    public UserController(UserService userService) {
+        this.userService = userService;
     }
 
     @GetMapping("/api/users")
     List<User> users() {
-        return repositoryConnector.findAll();
+        return userService.findAll();
     }
     @PostMapping("/api/users")
     User newUser(@RequestBody User user) {
-        return repositoryConnector.save(user);
+        return userService.save(user);
     }
 
     @GetMapping("/api/user/{id}")
     User singleUser(@PathVariable Long id) {
-        return repositoryConnector.getById(id);
+        return userService.getById(id);
     }
 
     @PutMapping("/api/user/{id}")
     User updateUser(@PathVariable Long id) {
-        User user = repositoryConnector.getById(id);
-        return repositoryConnector.save(user);
+        User user = userService.getById(id);
+        return userService.save(user);
     }
 
     @DeleteMapping("/api/user/{id}")
     void deleteUser(@PathVariable Long id) {
-        repositoryConnector.deleteById(id);
+        userService.deleteById(id);
     }
 }
